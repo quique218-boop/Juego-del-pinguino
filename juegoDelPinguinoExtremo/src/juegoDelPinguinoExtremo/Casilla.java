341a4627a82c4fd684278d7ab86ac5f12f82f608
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-24 16:07:45 +0100

    creacion casilla + tipos de casillas

---
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Casilla.java
@@ -0,0 +1,20 @@
+package juegoDelPinguinoExtremo;
+
+public abstract class Casilla {
+
+	private int posicion;
+	
+	public Casilla() {
+		
+	}
+	
+	public abstract void realizarAccion(Tablero tablero, Jugador jugador);
+	
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
+	}
+	
+	public int getPosicion() {
+		return this.posicion;
+	}
+}
---
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Trineo.java
@@ -0,0 +1,15 @@
+package juegoDelPinguinoExtremo;
+
+public class Trineo extends Casilla {
+
+	public Trineo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void realizarAccion(Tablero tablero, Jugador jugador) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
