4806e5af2c507d5d25c4d3567e9a78d77d64d674
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-24 16:40:29 +0100

    progreso en Tablero

---
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Normal.java
@@ -0,0 +1,15 @@
+package juegoDelPinguinoExtremo;
+
+public class Normal extends Casilla {
+
+	public Normal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void realizarAccion(Tablero tablero, Jugador jugador) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Tablero.java
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Tablero.java
@@ -1,18 +1,79 @@
 package juegoDelPinguinoExtremo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 public class Tablero {
 
 	private ArrayList<Jugador> listaJugador;
-	private ArrayList<Casilla> casillas;
+	private ArrayList<Casilla> listaCasillas;
 	private int turnos;
 	private int jugadorActual;
 	private boolean finalizada;
 	private Jugador ganador;
 
+	private Random ran = new Random();
+
 	public Tablero() {
 
+		listaJugador = new ArrayList<>();
+
+		listaCasillas = new ArrayList<>();
+
+	}
+
+	/*
+	 * for (int i = 0; i < 50; i++) {
+	 * 
+	 * switch (ran.nextInt(6)) {
+	 * 
+	 * case 0: casillas.add(new Oso()); break;
+	 * 
+	 * case 1: casillas.add(new Trineo()); break; case 2: casillas.add(new
+	 * Agujero()); break;
+	 * 
+	 * case 3: casillas.add(new Evento()); break;
+	 * 
+	 * case 4: casillas.add(new SueloQuebradizo()); break;
+	 * 
+	 * case 5: casillas.add(new Normal()); break;
+	 * 
+	 * } }
+	 * 
+	 * 
+	 */
+
+	public void addJugador(Jugador jugador) {
+
+		this.listaJugador.add(jugador);
+	}
+
+	public Jugador getJugador(int posicion) {
+
+		return this.listaJugador.get(posicion);
+	}
+
+	public void addCasilla(Casilla casilla) {
+
+		this.listaCasillas.add(casilla);
+
+	}
+
+	public Casilla getCasilla(int posicion) {
+
+		return this.listaCasillas.get(posicion);
+
+	}
+
+	public void setjugadorActual(int jugadorActual) {
+
+		this.jugadorActual = jugadorActual;
+	}
+
+	public int getjugadorActual() {
+
+		return this.jugadorActual;
 	}
 
 	public void setTurnos(int turnos) {
@@ -25,14 +86,14 @@ public class Tablero {
 		return this.turnos;
 	}
 
-	public void setjugadorActual(int jugadorActual) {
+	public void setFinalizada(boolean finalizada) {
 
-		this.jugadorActual = jugadorActual;
+		this.finalizada = finalizada;
 	}
 
-	public int getjugadorActual() {
+	public boolean getFinalizada() {
 
-		return this.jugadorActual;
+		return this.finalizada;
 	}
 
 	public void setGanador(Jugador ganador) {
