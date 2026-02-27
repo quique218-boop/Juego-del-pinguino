fe303892294bd911508db25444bd79c976b85aa3
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-27 18:02:58 +0100

    progreso casilla evento

--- juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Evento.java
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Evento.java
@@ -1,16 +1,48 @@
 package juegoDelPinguinoExtremo;
 
+import java.util.Random;
+
 public class Evento extends Casilla {
 
-	String[] eventos;
+	String[] eventos = { "Pez", "BNeu", "Dado"};
 
 	public Evento() {
 	}
 
 	@Override
 	public void realizarAccion(Tablero tablero, Jugador jugador) {
-		// TODO Auto-generated method stub
 
+		Random ran = new Random();
+
+		switch (eventos[ran.nextInt(eventos.length)]) {
+		
+		case "Pez": {
+			// dar pez
+		}
+
+		case "BNeu": {
+			//bolas de nieve += ran.nextInt(3) + 1;
+		}
+
+		case "Dado": {
+			
+			int valor = ran.nextInt(11);
+			
+			if(valor < 8) {
+				
+				// Dado rapido
+				
+			} else {
+				
+				// dado lento
+				
+			}
+			
+		}
+		
+		default:
+			System.out.println("Caso no valido");
+		}
 	}
 
 	public void setEventos(String[] eventos) {
