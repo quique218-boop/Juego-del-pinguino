dee03262241619cf30bb5ad63136355cabf1950f
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-27 19:50:12 +0100

    remove extra inv

--- juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Jugador.java
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Jugador.java
@@ -1,73 +1,74 @@
 package juegoDelPinguinoExtremo;
+
 import java.util.Scanner;
+
 public abstract class Jugador {
-	
-private int posicion;
-private String nombre;
-private String color;
-private Inventario inv;
-private boolean turnoTerminado;
-private Inventario inventario;
+
+	private int posicion;
+	private String nombre;
+	private String color;
+	private boolean turnoTerminado;
+	private Inventario inventario;
 
 	public Jugador(String nombre, String color, Inventario inventario) {
-		
+
 		this.nombre = nombre;
 		this.color = color;
 		this.posicion = 0;
 		this.inventario = inventario;
 	}
-	
+
 	public String getNombre() {
-		
+
 		return nombre;
-		
+
 	}
-	
+
 	public void setNombre(String nombre) {
-		
+
 		this.nombre = nombre;
-		
+
 	}
-	
+
 	public String getColor() {
-		
+
 		return color;
-		
+
 	}
-	
+
 	public void setColor(String color) {
-		
+
 		this.color = color;
-		
+
 	}
-	
+
 	public int getPos() {
-		
+
 		return posicion;
-		
+
 	}
-	
+
 	public void setPos(int posicion) {
-	
+
 		this.posicion = posicion;
 	}
-	
+
 	public void moverPosicion(int nDado) {
-		
-		this.posicion += nDado; //Sistema de avance del jugador
-		
+
+		this.posicion += nDado; // Sistema de avance del jugador
+
 	}
-	
-public Inventario getInventario() {
-		
+
+	public Inventario getInventario() {
+
 		return inventario;
-		
+
 	}
-	
+
 	public void setInventario(Inventario inventario) {
-		
+
 		this.inventario = inventario;
-		
+
 	}
 
 }
