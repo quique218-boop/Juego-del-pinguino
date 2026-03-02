cd3661d05fb6b9f59b850db5c5ae1d3668db5779
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-03-02 16:56:49 +0100

    esSobornado convertido en un toggle de boolean soborno

--- juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Foca.java
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Foca.java
@@ -1,75 +1,62 @@
 package juegoDelPinguinoExtremo;
 
-public class Foca extends Jugador{
-	
+public class Foca extends Jugador {
+
 	private boolean soborno;
-	
+
 	public Foca(String nombre, String color, Inventario inventario) {
-		
+
 		super(nombre, color, inventario);
-		
+
 		this.soborno = false;
-		
+
 	}
-	
+
 	public boolean getSoborno() {
-		
+
 		return this.soborno;
-		
+
 	}
-	
+
 	public void setSoborno(boolean soborno) {
-		
+
 		this.soborno = soborno;
-		
+
 	}
-	
-	public void aplastarJugador(Pinguino jugador) { 
-		
-	jugador.getInventario().RobarInventario(); //La foca roba la mitad del inventario del jugador
-		
+
+	public void aplastarJugador(Pinguino jugador) {
+
+		jugador.getInventario().RobarInventario(); // La foca roba la mitad del inventario del jugador
+
 	}
-	
+
 	public void golpearJugador(Pinguino jugador, Tablero tablero) {
-		
-		boolean encontrado = false;
-		
-		if(this.getPos() != jugador.getPos()) {
-			
-			return; //Si no estan en la misma casilla no pasa nada
-			
-		}else {
-			
-			System.out.println("La foca golpea al pinguino fuertemente");
-			
-			
+
+		if (this.getPos() != jugador.getPos()) {
+
+			return; // Si no estan en la misma casilla no pasa nada
+
+		} else {
+
+			boolean encontrado = false;
 
 			for (int i = jugador.getPos() - 1; i > 0 && encontrado == false; i--) {
 
 				if (tablero.getCasilla(i) instanceof Agujero) {
-					
+
 					jugador.setPos(i);
-					
+
 					encontrado = true;
-					
+
 				}
 			}
-			 
 		}
-		
 	}
-	
+
 	public void esSobornado() {
-		
-		System.out.println("La foca come el pez");
-		
-		for(int i = 2; i > 0; i--) {
-			
-			System.out.println("Foca bloqueada por " + i + " turnos");
-			
-		}
-		
-		
+
+		this.soborno = this.soborno ? false : true;
+
 	}
 
 }
