8f0b52ec4da71f919594b832cd2ef8ef06fd446b
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-03-03 16:57:40 +0100

    Integracion JavaFX

--- juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Main.java
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Main.java
@@ -1,9 +1,28 @@
 package juegoDelPinguinoExtremo;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
 
-	}
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(loader.load(), 400, 300);
+
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 
 }
