d768f6795f79f7f5af101dae55b7137fd8cc2ddb
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-03-03 18:07:43 +0100

    testing

--- juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Main.java
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Main.java
@@ -1,28 +1,48 @@
 package juegoDelPinguinoExtremo;
 
+import java.awt.TextField;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
+	@Override
+	public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("login.fxml"));
+		String javaVersion = System.getProperty("java.version");
+		String javafxVersion = System.getProperty("javafx.version");
+		
+		
+		Label l = new Label("LOGIN");
 
-        Scene scene = new Scene(loader.load(), 400, 300);
+	    PasswordField passwordfield = new PasswordField();//fx:id="passField" promptText="Password"
 
-        stage.setTitle("Login");
-        stage.setScene(scene);
-        stage.show();
-    }
+	    Button button = new Button("ENTER");
+	    
+	    StackPane stack = new StackPane();
+	    
+	    stack.getChildren().add(l);
+	    stack.getChildren().add(passwordfield);
+	    stack.getChildren().add(button);
+	    
+	    stack.setAlignment(stack, null);
+	    
 
-    public static void main(String[] args) {
-        launch();
-    }
+	    Scene scene = new Scene(stack, 640, 480);
+
+		stage.setScene(scene);
+		
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
