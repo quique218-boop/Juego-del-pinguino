d8aeb3865b0700e58d218cae9d2090671246494d
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-03-04 17:49:38 +0100

    Revert "testing"
    This reverts commit d768f6795f79f7f5af101dae55b7137fd8cc2ddb.

--- juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Main.java
+++ juegoDelPinguinoExtremo/src/juegoDelPinguinoExtremo/Main.java
@@ -1,48 +1,28 @@
 package juegoDelPinguinoExtremo;
 
-import java.awt.TextField;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.layout.StackPane;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
-	@Override
-	public void start(Stage stage) throws Exception {
+    @Override
+    public void start(Stage stage) throws Exception {
 
-		String javaVersion = System.getProperty("java.version");
-		String javafxVersion = System.getProperty("javafx.version");
-		
-		
-		Label l = new Label("LOGIN");
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("login.fxml"));
 
-	    PasswordField passwordfield = new PasswordField();//fx:id="passField" promptText="Password"
+        Scene scene = new Scene(loader.load(), 400, 300);
 
-	    Button button = new Button("ENTER");
-	    
-	    StackPane stack = new StackPane();
-	    
-	    stack.getChildren().add(l);
-	    stack.getChildren().add(passwordfield);
-	    stack.getChildren().add(button);
-	    
-	    stack.setAlignment(stack, null);
-	    
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
 
-	    Scene scene = new Scene(stack, 640, 480);
-
-		stage.setScene(scene);
-		
-		stage.show();
-	}
-
-	public static void main(String[] args) {
-		launch();
-	}
+    public static void main(String[] args) {
+        launch();
+    }
 
 }
