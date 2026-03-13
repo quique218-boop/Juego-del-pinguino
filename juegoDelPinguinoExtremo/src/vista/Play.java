73b120f6eeefb45b5da929d8cf5c253513153d97
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-03-13 17:37:59 +0100

    progresso tablero

--- juegoDelPinguinoExtremo/src/vista/Play.java
+++ juegoDelPinguinoExtremo/src/vista/Play.java
@@ -1,11 +1,43 @@
 package vista;
 
+import controlador.*;
 import javafx.fxml.FXML;
 
 public class Play {
+	
+	private GestorTablero gestorTablero;
+	
+	@FXML
+	private void initialize() {
+		
+		GestorTablero = new GestorTablero();
+	}
 
 	@FXML
-	private void Click() {
+	private void lanzar() {
+
+	}
+	
+	@FXML
+	private void usarBola() {
+
+	}
+	
+	@FXML
+	private void usarPez() {
+
+	}
+
+	@FXML
+	private void usarDadoR() {
+
+	}
+
+	@FXML
+	private void usarDadoL() {
+		
+		
+
 	}
 
 }
