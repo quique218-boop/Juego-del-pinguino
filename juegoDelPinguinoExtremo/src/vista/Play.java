eb227c70d8bd40102d0b265965cc080cc853d5bb
joseporiolse-creator <joseporiolse@alumnes.ilerna.com>
2026-03-16 16:29:09 +0100

    bolas de nieve

--- juegoDelPinguinoExtremo/src/vista/Play.java
+++ juegoDelPinguinoExtremo/src/vista/Play.java
@@ -2,7 +2,8 @@ package vista;
 
 import controlador.*;
 import javafx.fxml.FXML;
-
+import modelo.Pinguino;
+import modelo.*;
 public class Play {
 	
 	private GestorTablero gestorTablero;
@@ -25,7 +26,31 @@ public class Play {
 	@FXML
 	private void usarBola() {
 
-		//TODO
+		Pinguino pinguino = (Pinguino) gestorTablero.getPartida().getJugadorActual();
+		
+		Inventario inventario = pinguino.getInventario();
+
+			if (!inventario.getBolas().isEmpty()) { // Si la lista no es vacia
+
+				jugador.usarItem(inventario.getBolas().get(0)); // El jugador coge la primera bola de nieve de la lista
+				
+				System.out.println("A que jugador quieres lanzar-le la bola de nieve?");
+				
+				String el = scan.nextLine();
+				
+				gestorJugador.getPartida().getArrayList
+				
+				if(int skill = random.nextInt() + 1) {
+					
+					if(skill <= 5) {
+						
+						System.out.println();
+					}
+				}
+			}
+		
+		
+		
 		
 	}
 	
